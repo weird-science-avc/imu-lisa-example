27c86cbfb90edb6028364e051138f1e2c2627cc7
Andrew Taggart <andrew.taggart@sendgrid.com>
2015-06-20 09:04:47 -0600

    test cma

--- main.ino
+++ main.ino
@@ -1,16 +1,23 @@
 #include "stupidc.h"
-
+const int IMU_MEMORY = 20;
 Orientation orientation;
+unsigned long duration;
+float lastYaw;
 
 void setup() {
   Serial.begin(115200);
+  duration = millis();
 }
 
 void loop() {
   updateOrientation(orientation);
-  Serial.print("Orientation: "); Serial.print(orientation.pitch);
-  Serial.print("\t"); Serial.print(orientation.roll);
-  Serial.print("\t"); Serial.print(orientation.yaw); Serial.println();
+  if (millis() - duration >= 10) {
+    int cmaplus1 = (orientation.yaw + IMU_MEMORY * lastYaw)/(IMU_MEMORY + 1);
+    Serial.print("last yaw (10 ms ago): "); Serial.print(lastYaw);
+    Serial.print("current yaw: "); Serial.println(cmaplus1);
+    lastYaw = cmaplus1;
+    Serial.print("roll: "); Serial.print(orientation.roll); Serial.print(" roll: "); Serial.println(orientation.roll);
+  }
 }
 
 
